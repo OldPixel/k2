b9d22a75797acf6c20e218cad8627d364a38f3fc
Mariusz Tasak <pixelmt@gmail.com>
2011-06-03 11:34:46 +0200

    przerobiony program

--- kolo2.cpp
+++ kolo2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const unsigned stala = 1000;
 
 int main(int argc, char **argv) {
-  cout<<"Imie Nazwisko, grupa(sepcjalnosc), nr. indeksu"<<endl;
+  cout<<"Imie Nazwisko : "<<"Mariusz Tasak\n" << "grupa(sepcjalnosc) : "<<"ABiBD \n"<<" nr. indeksu : "<<"101592"<<endl;
   return 0;
 }
 
